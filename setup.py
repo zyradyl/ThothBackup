c7ad8d6bb573f119e00b84ea2b31607e050e11c5
Natalie Spencer <nmspencer@icloud.com>
2018-11-21 23:34:21 -0600

    Prepare to reserve name on PyPI and humiliate myself forever, lol.

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='thothclient',
       version='0.1',
       description='User client for thothbackup',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/zyradyl/thothclient',
       author='Zyradyl',
       author_email='nmspencer89@outlook.com',
       license='ISC',
       packages=['thothclient'],
-      zip_safe=False)
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: ISC License",
+        "Operating System :: OS Independent",
+      ])
