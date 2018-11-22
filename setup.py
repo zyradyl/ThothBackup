ca29e60f08679dd9305c75e8309e9a28ca11d2b5
Natalie Spencer <nmspencer@icloud.com>
2018-11-21 23:25:22 -0600

    Setup layout

---
+++ setup.py
@@ -0,0 +1,13 @@
+# Scaffolding
+
+from setuptools import setup
+
+setup(name='thothclient',
+      version='0.1',
+      description='User client for thothbackup',
+      url='https://github.com/zyradyl/thothclient',
+      author='Zyradyl',
+      author_email='nmspencer89@outlook.com',
+      license='ISC',
+      packages=['thothclient'],
+      zip_safe=False)
