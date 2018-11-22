cc76d2cce6e5fc674f6f5a0210dae5d908123f5e
Natalie Spencer <nmspencer@icloud.com>
2018-11-21 23:06:53 -0600

    Here we go with the rewrite already.
    Signed-off-by: Natalie Spencer <nmspencer@icloud.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+## ThothClient ##
+This repository houses the python client for the Thoth Backup system.
+More information will be made available as time permits.
---
+++ client.py
@@ -0,0 +1,12 @@
+#
+# File:         client.py
+# Title:        Thoth Backup Client (Python Version)
+# Author:       Zyradyl
+# Version:      0.01
+# Description:  This will eventually be a client for the Thoth Backup System,
+#               however it is also my first python project, and an ambitious
+#               one. This code is not likely to be the best looking code in
+#               the world, but such is life.
+#
+
+print("Hello, World!")
